b381c80aae9e744eba5d55e009a7812afcaf5d27
Umar Tanveer <151952538+not-noobcoder99@users.noreply.github.com>
2023-11-25 10:39:59 +0500

    Create Leap year finder.py

---
+++ Leap year finder.py
@@ -0,0 +1,5 @@
+year = int(input("Year number = "))
+if year % 4 == 0 :
+    print('This is a leap year and it will have 366 days!!!')
+else :
+    print('This is not a leap year and it will have normal 365 days.')
